3bd3a4747fbf7a16ee1b94eb4ae9e875c3cb1d48
Mohammed Sufiyan <ms9703303@gmail.com>
2026-09-12 22:38:38 +0530

    Restrict CORS to frontend

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+    "https://mindscape-1-s2d6.onrender.com"],
     allow_methods=["*"],
     allow_headers=["*"]
 )
